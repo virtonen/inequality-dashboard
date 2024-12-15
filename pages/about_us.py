d5a74164735af1c06005726c132b5727c1b0449e
Margaret Possidente <139285065+mle0na@users.noreply.github.com>
2024-12-15 12:37:45 +0100

    Delete pages as sidebar

--- pages/about_us.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📖 About Us")
-st.write("""
-Welcome to the **World Inequality Dashboard**!  
-Our mission is to provide a platform to explore global economic inequality trends interactively.  
-Developed for policymakers, researchers, and students who want to dive into the data!
-""")
