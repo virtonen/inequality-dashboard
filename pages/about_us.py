d4e29921ab81460e035faf33a42516fa1769730a
Margaret Possidente <139285065+mle0na@users.noreply.github.com>
2024-12-15 12:34:35 +0100

    Create about us

---
+++ pages/about_us.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("📖 About Us")
+st.write("""
+Welcome to the **World Inequality Dashboard**!  
+Our mission is to provide a platform to explore global economic inequality trends interactively.  
+Developed for policymakers, researchers, and students who want to dive into the data!
+""")
