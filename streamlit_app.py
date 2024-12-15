7a8ec8215dcf2e47984bea3bda2e613c97ebe317
Margaret Possidente <139285065+mle0na@users.noreply.github.com>
2024-12-15 21:19:46 +0100

    Update title

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,8 @@ if st.session_state.page == "Home":
         ## Table of Contents
         - [GDP Comparison](#gdp-comparison)
         - [Gini Coefficient](#gini-coefficient)""")
+        - [Poverty Headcount Ratio over time](#poverty-headcount-ratio-over-time)
+""")
 
 # -----------------------------------------------------------------------------
 # GINI DATA
