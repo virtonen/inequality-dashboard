7813fb48f5fbf6e2cbae47ddaca82c807f802bba
Margaret Possidente <139285065+mle0na@users.noreply.github.com>
2024-12-16 13:26:53 +0100

    Update who_is_this_for.py

--- navigation/who_is_this_for.py
+++ navigation/who_is_this_for.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def show_who_is_this_for():
-    st.title("### 👥 Who is This For?")
+    st.markdown("### 👥 Who is This For?")
     st.write("""
     This dashboard is for:
     - 🏛 **Policymakers**
